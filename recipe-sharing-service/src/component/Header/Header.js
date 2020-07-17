56d2e7c198a2384cfffa56775f76a8a46a87508e
shinyunsung <37016774+shinyunsung@users.noreply.github.com>
2020-07-18 02:00:42 +0900

    ADD Header

---
+++ recipe-sharing-service/src/component/Header/Header.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { Component } from "react";
+import styled from "styled-components";
+
+import Logo from "../../asets/Logo.png";
+
+class Header extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+  render() {
+    const Body = styled.div`
+      width: 100%;
+      height: 60px;
+
+      background: #ffffff;
+      border: 1px solid #e5e5e5;
+      box-sizing: border-box;
+    `;
+    const Wrapper = styled.div`
+      margin: 0 auto;
+      width: 940px;
+      height: 100%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    `;
+
+    return (
+      <Body>
+        <Wrapper>
+          <img src={Logo} alt="logo"></img>
+        </Wrapper>
+      </Body>
+    );
+  }
+}
+
+export default Header;
