cf350b3fdd5bb0b86d77f0e808178a325e4b24f7
shinyunsung <37016774+shinyunsung@users.noreply.github.com>
2020-07-19 21:23:12 +0900

    ADD axios in SerarchRecipe.jsx
    axios를 사용하여 더미데이터와의 통신

--- recipe-sharing-service/src/container/SearchRecipe/SearchRecipe.jsx
+++ recipe-sharing-service/src/container/SearchRecipe/SearchRecipe.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Component } from "react";
 import styled from "styled-components";
+import axios from "axios";
 
 import SearchBar from "../../component/SearchBar/SearchBar";
 import ListView from "../../component/ListView/ListView";
@@ -9,34 +10,29 @@ import Header from "../../component/Header/Header";
 class main extends Component {
   constructor(props) {
     super(props);
-
     this.state = {
-      List: {
-        ListName: [
-          {
-            name: "검색 결과",
-            Recipe: [
-              { Name: "1.고기 먹고 싶다.", Like: "100" },
-              { Name: "2.고기가 먹고 싶다.", Like: "200" },
-              { Name: "3.고기를 먹고 싶다.", Like: "300" },
-              { Name: "4.고기는 먹고 싶다.", Like: "400" },
-              { Name: "4.고기는 먹고 싶다.", Like: "400" },
-              { Name: "4.고기는 먹고 싶다.", Like: "400" },
-              { Name: "4.고기는 먹고 싶다.", Like: "400" },
-              { Name: "4.고기는 먹고 싶다.", Like: "400" },
-              { Name: "4.고기는 먹고 싶다.", Like: "400" },
-              { Name: "4.고기는 먹고 싶다.", Like: "400" },
-              { Name: "4.고기는 먹고 싶다.", Like: "400" },
-              { Name: "4.고기는 먹고 싶다.", Like: "400" },
-              { Name: "4.고기는 먹고 싶다.", Like: "400" },
-              { Name: "4.고기는 먹고 싶다.", Like: "400" },
-              { Name: "4.고기는 먹고 싶다.", Like: "400" },
-            ],
-          },
-        ],
-      },
+      SearchResultsList: [],
     };
   }
+
+  componentDidMount() {
+    this._getList();
+  }
+
+  _getList() {
+    const apiUrl = "dummy/SearchResultsList.json";
+    axios
+      .get(apiUrl)
+      .then((data) => {
+        this.setState({
+          SearchResultsList: data.data.SearchResultsList,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
   render() {
     const Wrapper = styled.div`
       margin: 0 auto;
@@ -48,7 +44,7 @@ class main extends Component {
         <Header></Header>
         <Wrapper>
           <SearchBar></SearchBar>
-          <ListView List={this.state.List.ListName[0]}></ListView>
+          <ListView List={this.state.SearchResultsList[0]}></ListView>
         </Wrapper>
       </div>
     );
